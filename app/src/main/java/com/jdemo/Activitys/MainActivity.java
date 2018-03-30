f1f9912bbae7b198d728e962bbe0ce599be125e9
jqsql <1517908593@qq.com>
2018-03-30 16:06:10 +0800

    18.3.30 更新

--- app/src/main/java/com/jdemo/Activitys/MainActivity.java
+++ app/src/main/java/com/jdemo/Activitys/MainActivity.java
@@ -1,7 +1,9 @@
-package com.jdemo;
+package com.jdemo.Activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
