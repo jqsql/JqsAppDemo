0489b481234624f596714ad51d4825a1b208865b
jqsql <1517908593@qq.com>
2018-03-30 14:59:32 +0800

    first

---
+++ app/src/main/java/com/jdemo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.jdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        initData();
+        initListen();
+
+    }
+
+    /**
+     * 初始化
+     */
+    private void initView() {
+
+    }
+
+    /**
+     * 数据处理
+     */
+    private void initData() {
+
+    }
+
+    /**
+     * 事件监听
+     */
+    private void initListen() {
+
+    }
+}
